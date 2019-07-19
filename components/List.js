1e373d76efd3fbea3f6e2026b654787a721a0536
ArthurBArre <arthur.barre@hetic.net>
2019-07-19 13:05:10 +0200

    conversion arr to LL and LL to arr ok

--- components/List.js
+++ components/List.js
@@ -1,7 +1,7 @@
 import React from 'react'; import { View } from 'react-native';
 class NewList extends React.Component { render() { return (<View></View>) } } export default NewList;
 
-const n = 3;
+const n = 10;
 const RANGE = 10;
 var length = 0
 
@@ -14,19 +14,19 @@ function head(node) {
 function tail(node) {
   if (node) return node.next;
 }
-// function generateList(n) {
-//   length = length + 1;
-//   let randomNumber = Math.floor(Math.random() * (RANGE))
-//   if (n > 0) return node(randomNumber, generateList(n - 1));
-//   else return null;
-// }
 function generateList(n) {
-  for (let i = 0; i < n; i++) {
-    var randomNumber = i;
-  }
+  length = length + 1;
+  let randomNumber = Math.floor(Math.random() * (RANGE))
   if (n > 0) return node(randomNumber, generateList(n - 1));
   else return null;
 }
+// function generateList(n) {
+//   for (let i = 0; i < n; i++) {
+//     var number = i;
+//   }
+//   if (n > 0) return node(number, generateList(n - 1));
+//   else return null;
+// }
 
 function print(node) {
   if (!node) console.log("end");
@@ -42,7 +42,6 @@ function count(node) {
   if (!node) return 0;
   return 1 + count(tail(node));
 }
-// var nb=count(list)
 function sum(node) {
   if (!node) return 0;
   return head(node) + sum(tail(node));
@@ -55,30 +54,82 @@ function conc(p, q) {
     return node(head(p), conc(tail(p), q))
   }
 }
+//
 function dup(p) {
   if (!p) {
-    return p = oldP;
+    return p;
   } else {
     return node(head(p), dup(tail(p)))
   }
 }
+//
 function append(p, a) {
   if (!p) {
-    return node(a, null)
+    return a
   }
   else {
-    node(head(p), append(tail(p), a))
+    return node(head(p), append(tail(p), a))
   }
 }
-var a = 1;
+//
+function appendAndSort(p, a) {
+  if (p) {
+    if (head(p) < a) {
+      return node(a, p)
+    }
+    else {
+      return node(head(p), appendAndSort(tail(p), a))
+    }
+  } else {
+    return node(a, null)
+  }
+}
+
+function convertObjToArr(obj) {
+  let arr2 = [];
+  while (obj != null) {
+    arr2.push(head(obj))
+    obj = tail(obj);
+  }
+  return arr2;
+}
+//
+var arr = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9
+];
+//
+function convertArrayToList(arr) {
+  var list = null;
+  var index = 0;
+//
+  while (index < arr.length) {  
+    list = node(arr[index], list);
+    index++;
+  }
+  return list;
+}
+var letest2 = convertArrayToList(arr);
+console.log('convertArrayToList: ', letest2);
+
+//list
+var obj = { "value": 1, "next": { "value": 12, "next": { "value": 3, "next": { "value": 4, "next": { "value": 5, "next": null } } } } };
 var p = generateList(n);
-const oldP = p
-//console.log('p: ', p);
-var q = generateList(n + 10)
-//console.log('q: ', q)
+var q = generateList(n + 10);
+//values
+const a = 12;
+const w = 8.5;
+//concatenate function
 var concVal = conc(p, q)
 console.log('concVal: ', concVal);
+//duplicate function 
 var dupVal = dup(p)
 console.log('dupVal: ', dupVal);
-var appendVal = append(p, a)
-console.log('appendVal : ' + appendVal)
+//append function
+var appendVal = append(p, a);
+console.log('appendVal: ', appendVal);
+//append and sort function 
+var appendAndSortVal = appendAndSort(p, w)
+console.log('appendAndSortVal: ', appendAndSortVal);
+//convert list to array
+var arr2 = convertObjToArr(obj);
+console.log('convert List to Array: ', arr2);
